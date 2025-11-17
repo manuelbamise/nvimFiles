9b5bc018ede685ec3a475923596bb5f9102674b5
manuelbamise <etobi1930@gmail.com>
2025-11-17 17:14:28 +0100

    changed the wait time before the donut screen saver shpws

--- lua/plugins/setup/donut.lua
+++ lua/plugins/setup/donut.lua
@@ -1,4 +1,4 @@
 require("donut").setup({
-	timeout = 60 * 4, -- set to 0 to disable (can still be triggered manually with :Donut command)
+	timeout = 360, -- set to 0 to disable (can still be triggered manually with :Donut command)
 	sync_donuts = false,
 })
