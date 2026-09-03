53f26b4cfcdd7fa1b7ab96351373954b33ccc117
manuelbamise <etobi1930@gmail.com>
2026-09-04 00:03:49 +0100

    routine: routine save

--- init.lua
+++ init.lua
@@ -6,8 +6,11 @@ require("config.lazy")
 
 
 
+
 -- set background to terminal
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
 -- Optional: also clear sidebars and floats
 vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+
+
