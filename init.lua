48c91f39997c2c6a3774c4ddabfcb3d46d945f65
manuelbamise <etobi1930@gmail.com>
2026-09-03 22:28:35 +0100

    routine: routine save

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
-require("config.keymaps")
-require("config.lazy")
-require("config.options")
-require("config.lsp")
 
-require("plugins.setup.lualine")
-require("plugins.setup.indentSetup")
-require("plugins.setup.autotagSetup")
-require("plugins.setup.donut")
-
-vim.cmd([[colorscheme gruvbox]])
--- lua/plugins/setup/donut.lua
+++
@@ -1,4 +0,0 @@
-require("donut").setup({
-	timeout = 360, -- set to 0 to disable (can still be triggered manually with :Donut command)
-	sync_donuts = true,
-})
