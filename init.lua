d57fc1bf74389c03a853e97480b6ef2cc79aa8a8
manuelbamise <etobi1930@gmail.com>
2026-09-04 11:21:49 +0100

    routine: routine save

--- init.lua
+++ init.lua
@@ -1,12 +1,10 @@
 require("config.lazy")
-   
 
-	
 -- set background to terminal
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 -- Optional: also clear sidebars and floats
-vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
 
 -- set the number of spaces a tab inputs in insert mode
 vim.opt.expandtab = true
@@ -18,14 +16,9 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.cmd("filetype plugin indent on")
 
-
-
 -- vim.opt.lineheight = 2
 
 -- :checkhealth vim.lsp (to check the status of the installed LSPs)
-
-
-
 
 -- vim.keymap.set('n','K',vim.lsp.buf.hover)
 
